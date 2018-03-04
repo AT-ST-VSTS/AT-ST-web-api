705596e6a35a114d214d65f43432df25e80e6345
CedricCazin <cedric.cazin@gmail.com>
2018-03-04 20:19:02 +0100

    feat(root): add test

---
+++ AT-ST-web-api.test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AT_ST_web_api_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
